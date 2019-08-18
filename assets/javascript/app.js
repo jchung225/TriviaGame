ad94180175d77040839e4ff3ea53c1c1b7c5cf09
Jim Chung <jchung225@gmail.com>
2019-08-17 21:17:27 -0400

    Layout put down problems with the Javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,75 @@
-$( document ).ready(function() {
 
-setTimeout(oneminute, 60*1000);
-setTimeout(timeUp , 60* 1000);
 
-function oneminute (){
-    $("#Time-Remaining").append("Time-Remaining: "+ oneminute);
-}
+var correctAnswers ="";
+var WrongAnsewers="";
+    
+var TimeRemaining = false;
+if (TimeRemaining) {
+    intervalId = setInterval(count, 1000);
+    clockRunning = true;
 
-function timeUp(){
-    $("#Time-Remaining").html("Times UP!");
-}
+    }
 
-});
+
+    if ("Q1" === "Q1A4"){
+        correctAnswers++;
+    }
+    else {
+        WrongAnsewers++;
+    }
+
+    if ("Q2" ==="Q2A1"){
+        correctAnswers++;
+    }
+    else {
+        WrongAnsewers++;
+    }
+
+    if ("Q3" ==="Q3A1"){
+        correctAnswers++;
+    }
+    else {
+        WrongAnsewers++;
+    }
+    
+    if ("Q4" ==="Q4A2"){
+        correctAnswers++;
+    }
+    else {
+        WrongAnsewers++;
+    }
+    
+    if ("Q5" ==="Q5A3"){
+        correctAnswers++;
+    }
+    else {
+        WrongAnsewers++;
+    }
+
+    if ("Q6" ==="Q6A1"){
+        correctAnswers++;
+    }
+    else {
+        WrongAnsewers++;
+    }
+    
+
+    if ("Q7" ==="Q7A4"){
+        correctAnswers++;
+    }
+    else {
+        WrongAnsewers++;
+    }
+
+    var submitbutton= document.getElementById("submit");
+
+    $("submit").on("click",function() {
+        alert("you got " +correctAnswers + "right");
+        alert("you got " + WrongAnsewers + "Wrong");
+    });
+
+  
+    
+    
+    
+
